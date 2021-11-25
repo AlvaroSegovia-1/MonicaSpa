9e18c3ded2a72f46a91a9e1ef4747a059fc12c47
Alvagit25 <alvarodesegovia@gmail.com>
2021-11-25 14:33:26 +0100

    css slider

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.swiper = new Swiper({
+/* window.swiper = new Swiper({
   el: ".slider__contenedor",
   slideClass: "slider__slide",
   createElements: true,
@@ -10,3 +10,18 @@ window.swiper = new Swiper({
   pagination: true, // poner position: relative en .slider
   navigation: true, // poner position: relative en .slider
 });
+ */
+
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
